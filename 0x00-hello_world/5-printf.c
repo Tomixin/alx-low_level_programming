c92cb3de24e3a8e3397344ce919fcfc146179e52
Tomixin <tosinoyinloye13@gmail.com>
2022-09-09 11:59:11 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-/**                                                                                                                                     
-* main - print the string in the put function                                                                                           
-*                                                                                                                                       
-* Description : using the main function                                                                                                 
-* this program prints "Programming is like building a multilingual puzzle                                                               
-* Return: 0
-*/                                                                                                                                      
-int main (void)                                                                                                                         
-{                                                                                                                                       
-           printf("with proper grammar, but the outcome is a place of art,\n");                                                         
-           return (0);                                                                                                                  
+/**                                                                                                                                   
+* main - print the string in the put function                                                                                         
+*                                                                                                                                     
+* Description : using the main function                                                                                               
+* this program prints "Programming is like building a multilingual puzzle                                                             
+* Return: 0                                                                                                                           
+*/                                                                                                                                    
+int main (void)                                                                                                                       
+{                                                                                                                                     
+           printf("with proper grammar, but the outcome is a place of art,\n");
+           return (0);
 }
